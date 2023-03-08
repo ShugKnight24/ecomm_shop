914415033d486622cff12f505eb46f6779224019
Shugmi Shumunov <SShumunov@gmail.com>
2023-03-08 15:51:18 -0500

    chore: Initialize front end

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+export const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <h1>E-Comm Shop</h1>
+      <p>Hello World</p>
+    </div>
+  )
+}
