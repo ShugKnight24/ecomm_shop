d0b38cbb37b0a21c6694f356de7ae56f60a031e0
Shugmi Shumunov <SShumunov@gmail.com>
2023-03-09 15:24:25 -0500

    chore: Initial linting and testing setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [checker({ typescript: true }), react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  build: {
+    outDir: "build",
+  },
 })
